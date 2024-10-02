a1e03666c70c01e4e9d8e63006d22a8eff82ec0c
romaindesson <romain.desson.02320@gmail.com>
2024-10-02 13:04:16 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { FrontPageUI } from './components/FrontPage/FrontPageUI'
+
+function App() {
+
+  return (
+    <FrontPageUI />
+  )
+}
+
+export default App
---
+++ src/components/FrontPage/FrontPageUI.tsx
@@ -0,0 +1,7 @@
+export const FrontPageUI = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
